84e175f31f6985f25c4fbbabb012b3337c62445d
Denis Flueraru <87847384+DenisFlueraru@users.noreply.github.com>
2026-01-31 18:19:05 +0200

    Add files via upload

--- create-checkout-session.js
+++ create-checkout-session.js
@@ -3,36 +3,8 @@ import Stripe from 'stripe';
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // Shipping fees mapping in cents
-const shippingFees = {
-  RO: 500,
-  DE: 1200,
-  HU: 1200,
-  GB: 2600,
-  AT: 1300,
-  BE: 2200,
-  FR: 1400,
-  PL: 1200,
-  ES: 1500,
-  LU: 2000,
-  DK: 2000,
-  CZ: 1000,
-  SK: 1000,
-  HR: 1000,
-  CY: 1000,
-  EE: 1500,
-  LV: 1500,
-  FI: 4000,
-  IE: 1800,
-  LT: 1500,
-  NL: 1700,
-  PT: 1400,
-  SI: 1000,
-  SE: 2500,
-  CH: 2500,
-  IT: 1400,
-  BG: 1000,
-  GR: 1000,
-  US: 2500,
+const shippingRateIDs = {
+  RO: 'shr_1SvgqkKCa5SQsj7OQ2VdkU0W', // Romania shipping rate
 };
 
 export default async function handler(req, res) {
